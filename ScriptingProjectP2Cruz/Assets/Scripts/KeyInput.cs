3fbcadd9de0433bda7c22c2dd0d404843d63c0b9
Josecruz777 <145380670+cdblast@users.noreply.github.com>
2025-03-28 14:52:41 -0700

    did scripting project
    did a scripting project to make the sphere fall down when not on the platform.

--- ScriptingProjectP2Cruz/Assets/Scripts/KeyInput.cs
+++ ScriptingProjectP2Cruz/Assets/Scripts/KeyInput.cs
@@ -6,46 +6,5 @@ using UnityEngine;
 
 public class g : MonoBehaviour
 {
-    public Image Graphic;
-    public Sprite Standard;
-    public Sprite downgfx;
-    public Sprite upgfx;
-    public Sprite heldgfx;
-    public Text boolDisplay1;
-    public Text boolDisplay2;
-    public Text boolDisplay3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Graphic.Sprite = Standard;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        bool down = Input.GetKeyDown(KeyCode.Space);
-        bool held = Input.GetKey(KeyCode.Space);
-        bool up = Input.GetKeyUp(KeyCode.Space);
-
-        if (down)
-        {
-            Graphic.Sprite = downgfx;
-        }
-        else if (held)
-            Graphic.Sprite = heldgfx;
-    }
-      else if (up)
-        {
-        graphic.sprite = upgfx;
-        }
-else
-{ 
-    graphic.sprite = Standard;
-}
-
-boolDisplay1.text = " " + down;
-boolDisplay2.text = " " + held;
-boolDisplay3.text = " " + held;
-}
 }
