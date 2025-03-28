8bd67dbf92a974e90f4a8ed5fbf74e471883f749
Josecruz777 <145380670+cdblast@users.noreply.github.com>
2025-03-28 13:59:29 -0700

    did a scrtipting
    scripting project

---
+++ ScriptingProjectP2Cruz/Assets/Scripts/YetAnotherScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetAnotherScript : MonoBehaviour
+{
+    public int numberOfplayerDeaths = 3;
+}
+// Start is called before the first frame update
+
+    // Update is called once per frame
+ 
